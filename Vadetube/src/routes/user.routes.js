a89bbfa9c07ba6fccebaa711f39b9314fa22e63a
Samir khan <itssameer75@gmail.com>
2024-03-18 22:14:58 -0700

    Add routes for user controllers

--- Vadetube/src/routes/user.routes.js
+++ Vadetube/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccesToken, registerUser } from "../controllers/user.controllers.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserCurrentProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccesToken,
+  registerUser,
+  updateAccoundDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -19,11 +31,21 @@ router.route("/register").post(
   registerUser,
 );
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //Secured routes
-router.route("/logout").post(verifyJwt, logoutUser)
-router.route("/refresh-token").post(refreshAccesToken)
-
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccesToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccoundDetails);
+router
+  .route("/edit-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/edit-cover-image")
+  .patch(verifyJwt, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJwt, getUserCurrentProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
