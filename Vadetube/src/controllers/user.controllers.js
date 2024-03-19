99f77f1aea6312a98d448f94e3ad10b3b3b03732
Samir khan <itssameer75@gmail.com>
2024-03-18 22:02:19 -0700

    Add getUserCurrentProfile and getWatchHistory endpoints

--- Vadetube/src/controllers/user.controllers.js
+++ Vadetube/src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generatAccessAndRefreshTokens = async (userId) => {
   try {
@@ -311,6 +312,7 @@ const updateUserAvatar = asyncHandler( async (req, res) => {
   .status(200)
   .json(new ApiResponse(200, user, "Avatar is successfully update"))
 })
+
 const updateUserCoverImage = asyncHandler( async (req, res) => {
   
   const coverImageLocalPath = req.file?.path
@@ -343,6 +345,127 @@ const updateUserCoverImage = asyncHandler( async (req, res) => {
 
 })
 
+const getUserCurrentProfile = asyncHandler( async (req, res) => {
+  
+  const {username} = req.params
+
+  if (!username?.trim()) {
+    throw new ApiError(400, "Username is missing!")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers"
+        },
+        channlSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: {$in: [req.user?._id, "subscribers.subscribe"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        email: 1,
+        username: 1,
+        subscribedToCount: 1,
+        channlSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+      }
+    }
+  ])
+  console.log("Channel:: ",channel);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exits")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, channel[0], "User channel fatched successfully"))
+
+
+})
+
+const getWatchHistory = asyncHandler( async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: mongoose.Schema.Types.ObjectId(req.user?._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user[0].watchHistory, "Watch history is successfully"))
+})
+
 
 
 export {
@@ -354,5 +477,7 @@ export {
   getCurrentUser,
   updateAccoundDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserCurrentProfile,
+  getWatchHistory
 };
